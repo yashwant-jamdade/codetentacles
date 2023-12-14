3e64416992d773a10f2a97f0938064781b3aee94
Ruchita <ruchitacodetentacles@gmail.com>
2023-12-14 10:27:50 +0530

    changes

--- src/pages/stepperform/skillsdetails.js
+++ src/pages/stepperform/skillsdetails.js
@@ -1,13 +1,5 @@
 import React from "react";
-import Select from 'react-select'
 export default function skillsdetails() {
-    const skills = [
-        { value: '1', label: 'Javascript' },
-        { value: '2', label: 'React' },
-        { value: '3', label: 'Angular' },
-        { value: '4', label: 'HTML' },
-        { value: '3', label: 'C' }
-      ]
     return (
      <>
        <div className="flex   w-full p-2 ">
@@ -16,15 +8,21 @@ export default function skillsdetails() {
                     <form action="/" method="post">
                       <div className="mb-4">
                             <label className="block mb-2 text-sm font-medium text-gray-700 text-left" for="firstName">
-                            Select Skills
+                            Skills
                             </label>
-                            <Select
-                                  className="basic-single text-left text-sm text-gray-700 border border-gray-200"
-                                  isMulti
-                                  classNamePrefix="select"
-                            options={skills} />
-                        </div>
-                     
+                            <div className="flex space-x-6 mb-4">
+                         <input  type="text" placeholder="Add Skills" className="w-full px-3 py-3 text-sm leading-tight text-gray-700 border border-gray-200 rounded appearance-none focus:outline-none focus:shadow-outline"/>
+                         <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Remove</button>
+                         </div>
+                         <div className="flex space-x-6 mb-4">
+                         <input  type="text" placeholder="Add Skills" className="w-full px-3 py-3 text-sm leading-tight text-gray-700 border border-gray-200 rounded appearance-none focus:outline-none focus:shadow-outline"/>
+                         <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Remove</button>
+                         </div>
+                         <button type="button" className="text-white bg-blue-700 text-left flex hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                                            Add Skills
+                                        </button>
+
+                     </div>
                     </form>
                 </div>
             </div>
